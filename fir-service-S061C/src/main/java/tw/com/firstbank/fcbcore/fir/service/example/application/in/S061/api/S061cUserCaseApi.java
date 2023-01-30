b1ab7f568705a1c9d2fdcc7f05b215409af468fd
simon81029168 <simon81029168@gmail.com>
2023-01-30 11:48:06 +0800

    fix

--- fir-service-S061C/src/main/java/tw/com/firstbank/fcbcore/fir/service/example/application/in/S061/api/S061cUserCaseApi.java
+++
@@ -1,8 +0,0 @@
-package tw.com.firstbank.fcbcore.fir.service.example.application.in.S061.api;
-
-import tw.com.firstbank.fcbcore.fcbframework.core.application.in.UseCaseApi;
-
-public abstract class S061cUserCaseApi implements
-	UseCaseApi<UpdateS061RequestCommand, UpdateS061ResponseCommand> {
-
-}
